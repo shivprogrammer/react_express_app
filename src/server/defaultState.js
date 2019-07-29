3f01ea305e7b169e51026773a1c00132d9c7db3f
shivprogrammer <shiv.unadkat@gmail.com>
2019-07-29 14:20:35 -0700

    added one complete schema to defaultState.js

--- src/server/defaultState.js
+++ src/server/defaultState.js
@@ -9,6 +9,16 @@ export const defaultState = {
         owner: "U1"
     }],
     tasks: [{
-        name:
+        name: "Do tests",
+        id: "T1",
+        group: "G1",
+        owner: "U1",
+        isComplete: false
+    }],
+    comments: [{
+        owner: "U1",
+        id: "C1",
+        task: "T1",
+        content: "Great work!!!"
     }]
 }
