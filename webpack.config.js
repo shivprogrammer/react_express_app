eab096dd9931930254c34bbdfd72601d989bdb5f
shivprogrammer <shiv.unadkat@gmail.com>
2019-07-28 18:47:26 -0700

    built out webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,10 @@ const path = require("path");
 
 module.exports = {
     mode: "development",
-    entry:
+    entry: path.resolve(__dirname, `src`, `app`),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    }
 }
