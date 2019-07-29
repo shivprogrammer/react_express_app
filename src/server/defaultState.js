dfeeaab3dc7a484dc3433b4bac281dab02c4c342
shivprogrammer <shiv.unadkat@gmail.com>
2019-07-28 19:37:23 -0700

    building out a new defaultState file for use with redux

---
+++ src/server/defaultState.js
@@ -0,0 +1,14 @@
+export const defaultState = {
+    users: [{
+        id: "U1",
+        name: "Dev"
+    }],
+    groups: [{
+        name: "To Do",
+        id: "G1",
+        owner: "U1"
+    }],
+    tasks: [{
+        name:
+    }]
+}
