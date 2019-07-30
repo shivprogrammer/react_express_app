09c8122d3338bf56a40aaf387411c88ba6d89023
shivprogrammer <shiv.unadkat@gmail.com>
2019-07-30 00:28:00 -0700

    redux logger working wiht 'add new' button click

--- src/app/components/TaskList.jsx
+++ src/app/components/TaskList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { requestTaskCreation } from "../store/mutations";
 
 export const TaskList = ({tasks, name, id, createNewTask}) => (
     <div>
@@ -32,6 +33,7 @@ const mapDispatchToProps = (dispatch, ownPropes) => {
     return {
         createNewTask(id) {
             console.log('Creating new task... ', id);
+            dispatch(requestTaskCreation(id));
         }
     }
 }
