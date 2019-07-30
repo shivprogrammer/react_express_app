441fe18ef68a9a902fbb82138373f8b3d4c2e752
shivprogrammer <shiv.unadkat@gmail.com>
2019-07-29 23:27:30 -0700

    updated Main component, added react-router-dom and history packages

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard';
+import { Router, Route } from 'react-router-dom'
 
 export const Main = () => (
-    <Provider store={store}>
-        <div>
-            {/*Dashboard goes here*/}
-            <ConnectedDashboard/>
-        </div>
-    </Provider>
+    <Router history={}>
+        <Provider store={store}>
+            <div>
+                {/*Dashboard goes here*/}
+                <ConnectedDashboard/>
+            </div>
+        </Provider>
+    </Router>
 )
