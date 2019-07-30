e3e8cc5bd2ea8e69319740bbaffee49ced2f04f0
shivprogrammer <shiv.unadkat@gmail.com>
2019-07-29 23:32:25 -0700

    history successfully working with dashboard url routing

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { ConnectedDashboard } from './Dashboard';
-import { Router, Route } from 'react-router-dom'
+import { Router, Route } from 'react-router-dom';
+import { history } from "../store/history";
 
 export const Main = () => (
-    <Router history={}>
+    <Router history={history}>
         <Provider store={store}>
             <div>
-                {/*Dashboard goes here*/}
-                <ConnectedDashboard/>
+                <Route
+                    exact
+                    path="/dashboard"
+                    render={()=>(<ConnectedDashboard/>)}
+                />
             </div>
         </Provider>
     </Router>
